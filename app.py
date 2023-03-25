cb086559ca4e67d8206240b7abbc754faf605c41
entbappy <entbappy73@gmail.com>
2023-03-25 20:08:44 +0600

    updated

--- app.py
+++ app.py
@@ -39,6 +39,7 @@ def predict_datapoint():
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0",port=8080)        
+    # app.run(host="0.0.0.0",port=8080)        
+    app.run(debug=True)        
 
 
